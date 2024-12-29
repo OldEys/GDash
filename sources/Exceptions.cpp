45067ac63269eb99137e491a6cf4e830cd976153
Babii Victor264 <victor-ionut.babii@s.unibuc.ro>
2024-12-29 17:45:09 +0200

    ierarhie de exceptii pentru texturi,sunet,font si input file + testele 4

--- sources/Exceptions.cpp
+++ sources/Exceptions.cpp
@@ -3,11 +3,13 @@
 Font_error::Font_error(const std::string &path) : Exception(std::string("Could not load the font ").append(path)) {
 }
 
-Texture_error::Texture_error(const std::string& path) : Exception(std::string("Could not load the texture ").append(path) ) {
+Texture_error::Texture_error(const std::string &path) : Exception(
+    std::string("Could not load the texture ").append(path)) {
 }
 
-Sound_error::Sound_error(const std::string& path) : Exception(std::string("Could not load the sound ").append(path) ) {
+Sound_error::Sound_error(const std::string &path) : Exception(std::string("Could not load the sound ").append(path)) {
 }
 
-InputFile_error::InputFile_error(const std::string& path) :Exception(std::string("Could not load the input file ").append(path) ) {
+InputFile_error::InputFile_error(const std::string &path) : Exception(
+    std::string("Could not load the input file ").append(path)) {
 }
