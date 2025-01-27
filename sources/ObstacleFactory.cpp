48d97c86b0564978ee586032eab66aaa0cd4d521
Babii Victor264 <victor-ionut.babii@s.unibuc.ro>
2025-01-27 16:32:59 +0200

    reparat buguri de coliziune + schimbare mecanism derivate + mutat factory-ul+testelee

--- sources/ObstacleFactory.cpp
+++ sources/ObstacleFactory.cpp
@@ -2,16 +2,15 @@
 
 #include <iostream>
 
-ObstacleFactory::ObstacleFactory() {
-    factoryMap = {
-        {"block", [](sf::Vector2f pos) { return Block(pos).clone(); }},
-        {"spike", [](sf::Vector2f pos) { return Spike(pos).clone(); }},
-        {"platform", [](sf::Vector2f pos) { return Platform(pos).clone(); }},
-        {"short", [](sf::Vector2f pos) { return Short_Spike(pos).clone(); }},
-        {"jumporb", [](sf::Vector2f pos) { return JumpOrb(pos).clone(); }},
-        {"final", [](sf::Vector2f pos) { return Final(pos).clone(); }}
-    };
-}
+ObstacleFactory::ObstacleFactory()
+    : factoryMap{
+            {"block", [](sf::Vector2f pos) { return Block(pos).clone(); }},
+            {"spike", [](sf::Vector2f pos) { return Spike(pos).clone(); }},
+            {"platform", [](sf::Vector2f pos) { return Platform(pos).clone(); }},
+            {"short", [](sf::Vector2f pos) { return Short_Spike(pos).clone(); }},
+            {"jumporb", [](sf::Vector2f pos) { return JumpOrb(pos).clone(); }},
+            {"final", [](sf::Vector2f pos) { return Final(pos).clone(); }}
+    } {}
 
 std::shared_ptr<Obstacle> ObstacleFactory::createObstacle(const std::string &type, const sf::Vector2f &position) const {
     auto obs = factoryMap.find(type);
